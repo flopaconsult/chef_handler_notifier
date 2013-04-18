9b451577078d4656841ee15387d857cef8ee9100
Florin Popa <flopaconsult@gmail.com>
2013-04-18 12:08:06 +0300

    changes from florin-wrk on Thu Apr 18 12:08:06 EEST 2013

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,7 @@ license          "All rights reserved"
 description      "Installs/Configures chef_handler_notifier"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
-depends 		"chef_handler"
+
+%w{ chef_handler }.each do |cookbook|
+  depends cookbook
+end
